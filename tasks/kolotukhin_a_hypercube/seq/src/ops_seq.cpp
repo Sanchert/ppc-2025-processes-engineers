1e186858e70fc70fb5ebc9dc6c725e4f5c086e41
Alexander Kolotukhin <154746573+Sanchert@users.noreply.github.com>
2025-12-20 19:15:32 +0300

    another one working version

--- tasks/kolotukhin_a_hypercube/seq/src/ops_seq.cpp
+++ tasks/kolotukhin_a_hypercube/seq/src/ops_seq.cpp
@@ -58,79 +58,82 @@ bool KolotukhinAHypercubeSEQ::ValidationImpl() {
 
 bool KolotukhinAHypercubeSEQ::PreProcessingImpl() {
   GetOutput().data.clear();
-  GetOutput() = {{}, -1, valid_};
+  GetOutput() = {
+      {},
+      -1,
+      valid_,
+  };
   return true;
 }
 
 bool KolotukhinAHypercubeSEQ::RunImpl() {
+  int world_rank = 0;
   int world_size = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  if ((world_size <= 0) || ((world_size & (world_size - 1)) != 0) || (GetInput().source < 0) ||
-      (GetInput().source > world_size - 1)) {
+
+  if ((GetInput().source < 0) || (GetInput().source > world_size - 1) || (GetInput().dest < 0) ||
+      (GetInput().dest > world_size - 1) || (world_size <= 0) || ((world_size & (world_size - 1)) != 0)) {
     valid_ = false;
-    GetOutput() = {std::vector<int>{}, -1, valid_};
   }
 
-  if (valid_) {
-    int world_rank = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    const auto &input = GetInput();
-    int source = input.source;
-    int dest = input.dest;
-
-    std::vector<int> data;
-    std::uint64_t data_size = 0;
-
-    if (world_rank == source) {
-      data.reserve(input.data_size);
-      for (std::size_t i = 0; i < input.data_size; i++) {
-        data.push_back(static_cast<int>(i) * 2 + 1);
-      }
-      data_size = data.size();
-    }
-
-    int dimensions = CalculateHypercubeDimension(world_size);
-    hypercube_comm_ = CreateHypercubeComm(dimensions);
-    int cube_rank = -1;
-    MPI_Comm_rank(hypercube_comm_, &cube_rank);
-
-    int current = source;
-
-    for (int dim = 0; dim < dimensions; dim++) {
-      if ((source ^ dest) & (1 << dim)) {
-        int next = current ^ (1 << dim);
-        if (cube_rank == current) {
-          PerformComputeLoad(10000);
-          MPI_Send(&data_size, 1, MPI_UINT64_T, next, 0, hypercube_comm_);
-          MPI_Send(data.data(), static_cast<int>(data_size), MPI_INT, next, 1, hypercube_comm_);
-        } else if (cube_rank == next) {
-          MPI_Recv(&data_size, 1, MPI_UINT64_T, current, 0, hypercube_comm_, MPI_STATUS_IGNORE);
-          data.resize(data_size);
-          MPI_Recv(data.data(), static_cast<int>(data_size), MPI_INT, current, 1, hypercube_comm_, MPI_STATUS_IGNORE);
-          PerformComputeLoad(10000);
-        }
-        current = next;
-        if (current == dest) {
-          break;
-        }
-      }
-    }
-
-    if (world_rank == dest) {
-      GetOutput().data = data;
-      GetOutput().process_id = dest;
-    } else if (world_rank == source) {
-      GetOutput().data = data;
-      GetOutput().process_id = source;
-    } else {
-      GetOutput().data = std::vector<int>{};
-      GetOutput().process_id = world_rank;
-    }
-
-    MPI_Comm_free(&hypercube_comm_);
+  if (!valid_) {
+    GetOutput().data = std::vector<int>{};
+    GetOutput().exec = valid_;
+    GetOutput().process_id = -1;
+    return true;
   }
-  MPI_Barrier(MPI_COMM_WORLD);
+
+  const auto &input = GetInput();
+  int source = input.source;
+  int dest = input.dest;
+
+  std::vector<int> data;
+  size_t data_size = 0;
+
+  if (world_rank == source) {
+    data_size = input.data_size;
+    data.resize(data_size);
+    for (size_t i = 0; i < data_size; i++) {
+      data[i] = static_cast<int>(i) * 2 + 1;
+    }
+  }
+
+  int dimensions = CalculateHypercubeDimension(world_size);
+  hypercube_comm_ = CreateHypercubeComm(dimensions);
+  int cube_rank = -1;
+  MPI_Comm_rank(hypercube_comm_, &cube_rank);
+
+  int current = source;
+
+  for (int dim = 0; dim < dimensions; dim++) {
+    if ((source ^ dest) & (1 << dim)) {
+      int next = current ^ (1 << dim);
+      if (cube_rank == current) {
+        PerformComputeLoad(150000);
+        MPI_Send(&data_size, 1, MPI_UINT64_T, next, 0, hypercube_comm_);
+        MPI_Send(data.data(), static_cast<int>(data_size), MPI_INT, next, 1, hypercube_comm_);
+      } else if (cube_rank == next) {
+        MPI_Recv(&data_size, 1, MPI_UINT64_T, current, 0, hypercube_comm_, MPI_STATUS_IGNORE);
+        data.resize(data_size);
+        MPI_Recv(data.data(), static_cast<int>(data_size), MPI_INT, current, 1, hypercube_comm_, MPI_STATUS_IGNORE);
+        PerformComputeLoad(150000);
+      }
+      current = next;
+      if (current == dest) {
+        break;
+      }
+    }
+  }
+
+  if (world_rank == dest || world_rank == source) {
+    GetOutput().data = data;
+  } else {
+    GetOutput().data = std::vector<int>{};
+  }
+  GetOutput().process_id = world_rank;
+
+  MPI_Comm_free(&hypercube_comm_);
   return true;
 }
 
