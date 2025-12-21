8fc5b244b57eeb14a2226a4228c7530515925828
Alexander Kolotukhin <154746573+Sanchert@users.noreply.github.com>
2025-12-21 21:46:05 +0300

    try seq

--- tasks/kolotukhin_a_hypercube/seq/src/ops_seq.cpp
+++ tasks/kolotukhin_a_hypercube/seq/src/ops_seq.cpp
@@ -1,5 +1,8 @@
 #include "kolotukhin_a_hypercube/seq/include/ops_seq.hpp"
 
+#include <cstddef>
+#include <vector>
+
 #include "kolotukhin_a_hypercube/common/include/common.hpp"
 
 namespace kolotukhin_a_hypercube {
@@ -11,19 +14,24 @@ KolotukhinAHypercubeSEQ::KolotukhinAHypercubeSEQ(const InType &in) {
 }
 
 bool KolotukhinAHypercubeSEQ::ValidationImpl() {
-  return true;
+  return GetOutput().data.empty();
 }
 
 bool KolotukhinAHypercubeSEQ::PreProcessingImpl() {
-  GetOutput().exec = false;
+  GetOutput().data.clear();
   return true;
 }
 
 bool KolotukhinAHypercubeSEQ::RunImpl() {
+  std::size_t data_size = GetInput().data_size;
+  if (GetOutput().data.empty()) {
+    GetOutput().data.resize(data_size, 1);
+  }
   return true;
 }
 
 bool KolotukhinAHypercubeSEQ::PostProcessingImpl() {
+  GetOutput().process_id = GetInput().dest;
   return true;
 }
 
